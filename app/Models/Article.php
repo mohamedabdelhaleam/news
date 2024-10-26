4dbc9ec140cd39a0f322c4e2e0ec88921afc953c
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-10-26 13:35:50 +0300

    Create Image Attr In Article Model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -11,13 +11,26 @@ class Article extends Model
 
     protected $guarded = [];
 
+    public function getImageAttribute()
+    {
+        if ($this->attributes['image'] !== null) {
+            return URL('storage/articles/images') . '/' . $this->attributes['image'];
+        }
+        return null;
+    }
+
     public function category()
     {
-        return $this->belongsTo(Category::class , 'category_id' , 'id');
+        return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
     public function user()
     {
-        return $this->belongsTo(Admin::class , 'author' , 'id');
+        return $this->belongsTo(Admin::class, 'author', 'id');
+    }
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class, 'category_id', 'id');
     }
 }

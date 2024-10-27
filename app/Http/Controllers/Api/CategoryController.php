0dbbabd82718d369a4682f6e6e0aed3b67e425dd
mohamedabdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-10-27 12:33:40 +0300

    Create Category Controller for Api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+
+    public function index(Request $request)
+    {
+        $categories = Category::all();
+    }
 }

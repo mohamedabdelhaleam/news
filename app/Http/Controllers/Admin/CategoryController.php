3b5d40817700ff9bebbad9610e9892609be8caeb
mohamedabdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-10-27 12:33:20 +0300

    Refactor Category Controller

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,7 +26,6 @@ class CategoryController extends Controller
     {
         return view("admin.categories.create");
     }
-
     public function store(StoreCategoryRequest $request)
     {
         try {
@@ -43,33 +42,26 @@ class CategoryController extends Controller
             return redirect()->back()->withInput()->with("error", "خطأ في تسجيل الفئة");
         }
     }
-
     public function edit(Category $category)
     {
         return view('admin.categories.edit', compact('category'));
     }
-
     public function update(UpdateCategoryRequest $request, Category $category)
     {
         try {
             $fileName = uploadImage($request, 'image', 'categories/images') ?? basename($category->image);
             $slug = generateSlug($request->name) ?? $category->slug;
-            $successUpdated = $category->update([
+            $category->update([
                 'name' => $request->name ?? $category->name,
                 'description' => $request->description ?? $category->description,
                 'image' => $fileName,
                 'slug' => $slug,
             ]);
-            if (!$successUpdated) {
-                return redirect()->back()->withInput()->with("error", "خطأ في تعديل الفئة");
-            }
             return redirect()->route("dashboard.categories.index")->with("success", "تم تعديل بيانات الفئة بنجاح");
         } catch (\Exception $e) {
             return redirect()->back()->withInput()->with("error", "خطأ في تعديل الفئة");
         }
     }
-
-
     public function destroy(Category $category)
     {
         $category->delete();

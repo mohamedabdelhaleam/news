602d9fc65d5be7a61ed8aafa9ab8eb8396c9fa44
mohamedabdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-10-28 10:53:03 +0300

    Refactor Admin Route

--- routes/admin.php
+++ routes/admin.php
@@ -27,33 +27,8 @@ Route::get('/', function () {
 Route::prefix('admin')->name('dashboard.')->middleware(['auth:admin'])->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('admin');
 
-    Route::group(['prefix' => 'roles', 'as' => 'roles.'], function () {
-        Route::get("/", [RoleController::class, 'index'])->name('index');
-        Route::get("/add", [RoleController::class, 'create'])->name('add');
-        Route::post("/store", [RoleController::class, 'store'])->name('store');
-        Route::get('/{role}/edit', [RoleController::class, 'edit'])->name('edit');
-        Route::delete('/role/{role}', [RoleController::class, 'destroy'])->name('destroy');
-        Route::patch('/{role}', [RoleController::class, 'update'])->name('update');
-    });
-
-
-    Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
-        Route::get("/", [CategoryController::class, 'index'])->name('index');
-        Route::get("/add", [CategoryController::class, 'create'])->name('add');
-        Route::post("/store", [CategoryController::class, 'store'])->name('store');
-        Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('edit');
-        Route::delete('/category/{category}', [CategoryController::class, 'destroy'])->name('destroy');
-        Route::patch('/{category}', [CategoryController::class, 'update'])->name('update');
-    });
-
-
-    Route::group(['prefix' => 'articles', 'as' => 'articles.'], function () {
-        Route::get("/", [ArticleController::class, 'index'])->name('index');
-        Route::get("/add", [ArticleController::class, 'create'])->name('add');
-        Route::post("/store", [ArticleController::class, 'store'])->name('store');
-        Route::get('/{article}/edit', [ArticleController::class, 'edit'])->name('edit');
-        Route::delete('/article/{article}', [ArticleController::class, 'destroy'])->name('destroy');
-        Route::patch('/{article}', [ArticleController::class, 'update'])->name('update');
-    });
+    Route::resource('categories', CategoryController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('articles', ArticleController::class);
 
 });

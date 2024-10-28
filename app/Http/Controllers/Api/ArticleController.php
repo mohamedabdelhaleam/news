ad97ce6c0df38c63eb5adf4c8caf567177ebe4f2
mohamedabdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-10-28 11:32:07 +0300

    Create Article Api's

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $articles = Article::with(['user', 'category'])->get();
+            if (!$articles) {
+                return errorResponse('لا يوجد بيانات');
+            }
+            return successResponse($articles);
+        } catch (\Throwable $th) {
+            return errorResponse('خطاء في تنفيذ العملية');
+        }
+    }
+
+    public function show(Article $article)
+    {
+        try {
+            // Load user and category relationships
+            $article->load(['user', 'category']);
+
+            return successResponse($article);
+        } catch (\Throwable $th) {
+            return errorResponse('خطاء في تنفيذ العملية');
+        }
+    }
 }

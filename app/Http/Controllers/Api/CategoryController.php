cdc37a2e02291230a34fff48d441a5795c1794e6
mohamedabdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-10-28 11:32:17 +0300

    Create CAtegory Api's

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -4,13 +4,28 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Category;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
 
-    public function index(Request $request)
+    public function index()
     {
-        $categories = Category::all();
+        try {
+            $categories = Category::all();
+            if (!$categories) {
+                return errorResponse('لا يوجد بيانات');
+            }
+            return successResponse($categories);
+        } catch (\Throwable $th) {
+            return errorResponse('خطاء في تنفيذ العملية');
+        }
+    }
+
+    public function show(Category $category){
+        try {
+            return successResponse($category);
+        } catch (\Throwable $th) {
+            return errorResponse('خطاء في تنفيذ العملية');
+        }
     }
 }

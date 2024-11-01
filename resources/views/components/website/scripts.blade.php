173613fb2c832c27c2b8712243073c0b4d43a139
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-11-01 22:22:20 +0200

    create script blade for website

---
+++ resources/views/components/website/scripts.blade.php
@@ -0,0 +1,25 @@
+{{-- <script src="{{ asset('website/js/app-head-bs.js') }}"></script> --}}
+{{-- <script src="{{ asset('website/js/app.js') }}"></script> --}}
+{{-- <script src="{{ asset('website/js/uikit-components-bs.js') }}"></script> --}}
+
+
+{{-- <script  src="{{ asset('website/js/libs/jquery.min.js') }}"></script>
+<script src="{{ asset('website/js/libs/scrollmagic.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('website/js/libs/swiper-bundle.min.js') }}"></script> --}}
+{{-- <script src="{{ asset('website/js/libs/anime.min.js') }}"></script> --}}
+<script src="{{ asset('website/js/helpers/data-attr-helper.js') }}"></script>
+<script src="{{ asset('website/js/helpers/swiper-helper.js') }}"></script>
+<script src="{{ asset('website/js/helpers/anime-helper.js') }}"></script>
+<script src="{{ asset('website/js/helpers/anime-helper-defined-timelines.js') }}"></script>
+<script src="{{ asset('website/js/uni-core/js/uni-core-bundle.min.js') }}"></script>
+
+
+
+<link rel="preload" href="{{ asset('website/js/app-head-bs.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/libs/scrollmagic.min.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/libs/jquery.min.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/app.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/uikit-components-bs.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/libs/swiper-bundle.min.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/libs/anime.min.js') }}" as="script">
+<link rel="preload" href="{{ asset('website/js/app.js') }}" as="script">

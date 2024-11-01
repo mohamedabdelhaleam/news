75564ed60327e4e9f4a49ddb5d33e4360e0e8f0b
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-11-01 22:23:07 +0200

    create website layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,53 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME') }}</title>
 
-    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-
-    @include('components.css')
+    @include('components.website.css')
 
     @yield('css')
 
 </head>
 
-<body class="layout-light side-menu">
-    <div class="mobile-author-actions"></div>
-    @include('components.header')
-    <main class="main-content">
-
-        @include('components.sidebar')
-
+<body>
+    @include('components.website.header')
+    <main>
         @yield('content')
-
-        @include('components.footer')
-
     </main>
-    <div id="overlayer">
-        <div class="loader-overlay">
-            <div class="dm-spin-dots spin-lg">
-                <span class="spin-dot badge-dot dot-primary"></span>
-                <span class="spin-dot badge-dot dot-primary"></span>
-                <span class="spin-dot badge-dot dot-primary"></span>
-                <span class="spin-dot badge-dot dot-primary"></span>
-            </div>
-        </div>
-    </div>
-    <div class="overlay-dark-sidebar"></div>
-    <div class="customizer-overlay"></div>
-
-    @include('components.scripts')
-    <script>
-        $(document).ready(function() {
-            @if (session('success'))
-                toastr.success("{{ session('success') }}");
-            @endif
-
-            @if (session('error'))
-                toastr.error("{{ session('error') }}");
-            @endif
-        });
-    </script>
     @yield('script')
+    @include('components.website.scripts')
 </body>
 
 </html>

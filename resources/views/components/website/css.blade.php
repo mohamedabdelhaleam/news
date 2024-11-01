1c7cf7b935066712dff22cdb1da7d5784e6afb02
Mohamed Abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-11-01 22:22:09 +0200

    create css blade for website

---
+++ resources/views/components/website/css.blade.php
@@ -0,0 +1,8 @@
+
+
+<link rel="preload" rel="stylesheet" href="{{ asset('website/css/main.min.css') }}">
+<link rel="preload" rel="stylesheet" href="{{ asset('website/css/prettify.min.css') }}">
+<link rel="preload" rel="stylesheet" href="{{ asset('website/css/swiper-bundle.min.css') }}">
+<link rel="preload" rel="stylesheet" href="{{ asset('website/css/unicons.min.css') }}">
+<link rel="preload" rel="stylesheet" href="{{ asset('website/js/uni-core/css/uni-core.min.css') }}">
+
